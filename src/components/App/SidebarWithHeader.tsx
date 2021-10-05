2b7ae143809cd15ea3869f9d5cd928c4467a67c0
rmil <rhys.milling@googlemail.com>
2021-10-05 02:30:49 +0100

    Fix sidebar links

--- src/components/App/SidebarWithHeader.tsx
+++ src/components/App/SidebarWithHeader.tsx
@@ -50,78 +50,93 @@ interface LinkItemProps {
   icon?: IconType;
   name: string;
   to: string;
+  external: boolean;
 }
 
 const LinkItems: Array<LinkItemProps> = [
   {
     name: "Home",
     icon: FiHome,
-    to: "",
+    to: "/",
+    external: false,
   },
   {
     name: "Slack",
     icon: FiSlack,
     to: "https://ystv.slack.com",
+    external: true,
   },
   {
     name: "Trello",
     icon: FiTrello,
     to: "https://trello.com/ystv",
+    external: true,
   },
   {
     name: "Quotes Board",
     icon: FiAlignCenter,
     to: "/quotes",
+    external: false,
   },
   {
     name: "Webcams",
     icon: FiVideo,
     to: "/webcams",
+    external: false,
   },
   {
     name: "Webmail",
     icon: FiMail,
     to: "https://webmail.ystv.co.uk",
+    external: true,
   },
   {
     name: "Equipment Booking",
     icon: FiClipboard,
     to: "https://dash.adam-rms.com",
+    external: true,
   },
   {
     name: "Vault",
     icon: FiKey,
     to: "https://vault.ystv.co.uk",
+    external: true,
   },
   {
     name: "Creator Studio",
     icon: FiTv,
     to: `${process.env.REACT_ENV_CREATOR_BASEURL}`,
+    external: true,
   },
   {
     name: "History Wiki",
     icon: FiBook,
     to: "https://wiki.ystv.co.uk",
+    external: true,
   },
   {
     name: "Documentation Wiki",
     icon: FiBook,
     to: "https://docs.ystv.co.uk",
+    external: true,
   },
   {
     name: "Welcome Pages",
     icon: FiBook,
     to: "https://welcome.ystv.co.uk",
+    external: true,
   },
   {
     name: "Tech Blog",
     icon: FiMessageCircle,
     to: "https://medium.com/ystv",
+    external: true,
   },
   {
     name: "Constitution & Policy",
     icon: FiBook,
     to: "https://docs.ystv.co.uk/wiki/YSTV_Constitution",
+    external: true,
   },
 ];
 
@@ -129,10 +144,35 @@ interface NavItemProps extends FlexProps {
   // eslint-disable-next-line react/require-default-props
   icon?: IconType;
   to: string;
+  external: boolean;
   children: ReactText;
 }
 
-const NavItem = ({ icon, to, children, ...rest }: NavItemProps) => (
+const NavItem = ({ icon, to, external, children, ...rest }: NavItemProps) =>
+  external ? (
+    <a rel="noreferrer" href={to}>
+      <Flex
+        align="center"
+        p="4"
+        mx="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{ bg: "cyan.400", color: "white" }}
+        {...rest}
+      >
+        {icon && (
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{ color: "white" }}
+            as={icon}
+          />
+        )}
+        {children}
+      </Flex>
+    </a>
+  ) : (
     <Link to={to} style={{ textDecoration: "none" }}>
       <Flex
         align="center"
@@ -145,12 +185,17 @@ const NavItem = ({ icon, to, children, ...rest }: NavItemProps) => (
         {...rest}
       >
         {icon && (
-          <Icon mr="4" fontSize="16" _groupHover={{ color: "white" }} as={icon} />
+          <Icon
+            mr="4"
+            fontSize="16"
+            _groupHover={{ color: "white" }}
+            as={icon}
+          />
         )}
         {children}
       </Flex>
     </Link>
-);
+  );
 
 interface SidebarProps extends BoxProps {
   onClose: () => void;
@@ -175,7 +220,12 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => (
       <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
     </Flex>
     {LinkItems.map((link) => (
-      <NavItem key={link.name} icon={link.icon} to={link.to}>
+      <NavItem
+        key={link.name}
+        icon={link.icon}
+        to={link.to}
+        external={link.external}
+      >
         {link.name}
       </NavItem>
     ))}
@@ -270,7 +320,7 @@ export default function SidebarWithHeader({
 }): JSX.Element {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
+    <Box minH="100vh" bg={useColorModeValue("gray.100", "gray.900")}>
       <SidebarContent
         onClose={() => onClose}
         display={{ base: "none", md: "block" }}
