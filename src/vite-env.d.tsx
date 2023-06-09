54075b608384fe8d763b429014634fc8ef1b9fc0
Marks Polakovs <marks@markspolakovs.me>
2023-06-09 12:06:01 +0100

    Fixups

--- src/vite-env.d.tsx
+++ src/vite-env.d.tsx
@@ -1,19 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { ColorModeScript } from "@chakra-ui/react";
-import * as serviceWorker from "./serviceWorker";
-import App, { chakraTheme } from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <ColorModeScript initialColorMode={chakraTheme.config.initialColorMode} />
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/// <reference types="vite/client" />
+export {};
